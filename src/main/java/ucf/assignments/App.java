1778ced8cd8956315a1fb1cc3ecb8467379102be
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-07-05 13:51:28 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/App.java
@@ -0,0 +1,29 @@
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class App extends Application{
+
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../../resources/UI1.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("To-do list");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+
+
---
+++ src/main/java/ucf/assignments/FXMLController.java
@@ -0,0 +1,21 @@
+package ucf.assignments;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+
+public class FXMLController implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+
+    }
+}
